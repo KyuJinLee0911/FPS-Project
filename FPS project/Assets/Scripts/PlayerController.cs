1c09e02a74648ad5bb40e12886d425276ac2aa41
KyuJinLee0911 <rbwls1001@naver.com>
2024-06-18 18:39:08 +0900

    2024.06.18
    - 프로젝트 생성
    - 기본 캐릭터 컨트롤 제작

---
+++ FPS project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FPS.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] GameObject firstPersonCameraObject;
+
+        [Header("Look")]
+        Vector2 lookInputValue;
+        float yRotation = 0f;
+        float xRotation = 0f;
+
+        // 플레이어 시점에서 x축과 y축의 감도
+        // 에디터 상의 카메라 기준 x축, y축이 아님
+        // 즉, x축감도는 가로 회전(y축 회전)의 감도, y축 감도는 세로 회전(x축 회전)의 감도임
+        public float playerXAxisSensitivity = 1f;
+        public float playerYAxisSensitivity = 1f;
+
+        [Header("Move")]
+        Vector2 moveInputValue;
+        [SerializeField] float moveSpeed = 5f;
+        [SerializeField] float runningSpeedModifier = 1.5f;
+
+        [Header("Jump")]
+        [SerializeField] float jumpPower = 5f;
+        [SerializeField] int maxJumpCount = 2;
+        [SerializeField] int currentJumpCount;
+        [SerializeField] LayerMask groundLayer;
+
+        private void OnCollisionEnter(Collision other) 
+        {
+            ContactPoint contact = other.GetContact(0);
+            Vector3 pos = contact.point;
+
+            // 플레이어와의 충돌이 일어난 것은 무시
+            if(other.collider.tag == "Player") return;
+
+            // 캐릭터가 몬스터와 충돌이 일어났을 때
+            if(other.collider.tag == "Enemy")
+            {   
+                // 충돌이 일어난 지점의 y좌표가 캐릭터의 y좌표보다 위에 있을 때
+                // 즉, 발로 밟지 않았고 그냥 부딪혔을 때는 무시
+                if(pos.y > transform.position.y) return;
+                // 발로 밟았을 때 추가 예정
+            }
+            
+            // 점프 횟수 초기화
+            currentJumpCount = maxJumpCount;
+        }
+
+        private void Start()
+        {
+            currentJumpCount = maxJumpCount;
+        }
+
+        void OnMove(InputValue value)
+        {
+            moveInputValue = value.Get<Vector2>();
+        }
+
+        void OnLook(InputValue value)
+        {
+            lookInputValue = value.Get<Vector2>();
+        }
+
+        void OnJump()
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+            if (currentJumpCount > 0)
+            {
+                currentJumpCount--;
+                rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+        }
+
+        void OnRun(InputValue value)
+        {
+            // 누르기 시작할 떄 속도를 달리기 속도로
+            if (value.Get<float>() == 1)
+                moveSpeed *= runningSpeedModifier;
+            // 뗄 때 속도를 다시 원래대로
+            else if (value.Get<float>() == 0)
+                moveSpeed /= runningSpeedModifier;
+        }
+
+        void FixedUpdate()
+        {
+            Look();
+            Move();
+            
+        }
+        private void Look()
+        {
+            yRotation += lookInputValue.x * playerXAxisSensitivity;
+            xRotation -= lookInputValue.y * playerYAxisSensitivity;
+
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            // 상하로 돌릴때는 고개만
+            firstPersonCameraObject.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            // 좌우로 돌릴때는 몸이 같이
+            transform.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+
+        private void Move()
+        {
+            Vector3 movementVec = transform.forward * moveInputValue.y + transform.right * moveInputValue.x;
+            transform.position += movementVec.normalized * moveSpeed * Time.deltaTime;
+        }
+    }
+}
+
